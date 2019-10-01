0a022773ad54a48c2d375476c0dea3a0135755c7
Gee-Wey (Oct 7, London) Yue <geewey@gmail.com>
2019-10-01 15:20:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,37 +4,20 @@ require "pry"
 
 def load_library(file_path)
   # code goes here
-    
   emoticons = YAML.load_file(file_path)
-
-=begin
-  emoticons = {"angel"=>["O:)", "☜(⌒▽⌒)☞"],
-  "angry"=>[">:(", "ヽ(ｏ`皿′ｏ)ﾉ"],
-  "bored"=>[":O", "(ΘεΘ;)"],
-  "confused"=>["%)", "(゜.゜)"],
-  "embarrased"=>[":$", "(#^.^#)"],
-  "fish"=>["><>", ">゜))))彡"],
-  "glasses"=>["8D", "(^0_0^)"],
-  "grinning"=>["=D", "(￣ー￣)"],
-  "happy"=>[":)", "(＾ｖ＾)"],
-  "kiss"=>[":*", "(*^3^)/~☆"],
-  "sad"=>[":'(", "(Ｔ▽Ｔ)"],
-  "surprised"=>[":o", "o_O"],
-  "wink"=>[";)", "(^_-)"]}
-=end
  
   emos_hash = {
     "get_meaning" => {},
     "get_emoticon" => {}
   }
   
-  emoticons.each do |meaning, symbols|
-    emos_hash["get_meaning"] = {emos_hash[symbols][1] => emos_hash[meaning]}
-    emos_hash["get_emoticon"] = {emos_hash[symbols][0] => emos_hash[symbols][1]}
+  emoticons.each do |meaning, emoticon|
+    emos_hash["get_meaning"] = {emos_hash[emoticon][1] => emos_hash[meaning]}
+    emos_hash["get_emoticon"] = {emos_hash[emoticon][0] => emos_hash[emoticon][1]}
   
     # emos_hash[meaning]  #=> meaning “angel”
-    # emos_hash[symbols][1]  #=> japanese emoticon "☜(⌒▽⌒)☞"
-    # emos_hash[symbols][0]  #=> english emoticon "O:)"
+    # emos_hash[emoticon][1]  #=> japanese emoticon "☜(⌒▽⌒)☞"
+    # emos_hash[emoticon][0]  #=> english emoticon "O:)"
 
   end
   
