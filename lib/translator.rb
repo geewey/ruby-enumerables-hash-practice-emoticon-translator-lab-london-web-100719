aba998f85c1abd1b876849743802fefaa24378b1
Gee-Wey (Oct 7, London) Yue <geewey@gmail.com>
2019-10-01 15:28:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,29 +1,20 @@
 # require modules here
 require "yaml"
-require "pry"
 
 def load_library(file_path)
-  # code goes here
-  emoticons = YAML.load_file("./" + "#{file_path}")
- 
-  emos_hash = {
-    "get_meaning" => {},
-    "get_emoticon" => {}
+  emoticons = YAML.load_file(file_path)
+  
+  new_hash = {
+    'get_meaning' => {},
+    'get_emoticon' => {}
   }
-  
-  # binding.pry
-  
-  emoticons.each do |meaning, emoticon|
-    emos_hash["get_meaning"][emoticon][1] = emos_hash[meaning]
-    emos_hash["get_emoticon"][emoticon][0] = emos_hash[emoticon][1]
-  
-    # emos_hash[meaning]  #=> meaning “angel”
-    # emos_hash[emoticon][1]  #=> japanese emoticon "☜(⌒▽⌒)☞"
-    # emos_hash[emoticon][0]  #=> english emoticon "O:)"
+
+  emoticons.each do |key,value|
+    new_hash['get_emoticon'][value[0]] = emoticons[key][1]
+    new_hash['get_meaning'][value[1]] = key
   end
-  
-  
-  emos_hash
+
+  new_hash
 end
   
 def get_japanese_emoticon(file_path, emoticon)
