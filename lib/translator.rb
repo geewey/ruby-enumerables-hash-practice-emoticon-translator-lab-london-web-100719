32f588873606d2dcbd15cd5c23bedf7ea311fbac
Gee-Wey (Oct 7, London) Yue <geewey@gmail.com>
2019-10-01 15:46:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -40,6 +40,7 @@ end
   
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here
+  # take a traditional Western emoticon, like :) and translate it to its Japanese version
   translate = load_library(file_path)[emoticon]
   
 end
