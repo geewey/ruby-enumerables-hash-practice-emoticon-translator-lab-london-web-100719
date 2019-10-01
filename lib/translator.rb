5ea37b32ecc101eb47ab1f1cd21d7e30cfaecfdf
Gee-Wey (Oct 7, London) Yue <geewey@gmail.com>
2019-10-01 15:17:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,7 +29,8 @@ def load_library(file_path)
   }
   
   emoticons.each do |meaning, symbols|
-    emos_hash["get_meaning"] => {emos_hash[meaning] => 
+    emos_hash["get_meaning"] => {emos_hash[symbols][1] => emos_hash[meaning]}
+    emos_hash[]
     emos_hash[symbols][1] #japanese emoticon
     emos_hash[symbols][0] # english emoticon
   end
