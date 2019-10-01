e9f6a581dd245ef574a75a75c8682dbbd4cca40e
Gee-Wey (Oct 7, London) Yue <geewey@gmail.com>
2019-10-01 15:27:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require "pry"
 
 def load_library(file_path)
   # code goes here
-  emoticons = YAML.load_file(file_path)
+  emoticons = YAML.load_file("./" + "#{file_path}")
  
   emos_hash = {
     "get_meaning" => {},
